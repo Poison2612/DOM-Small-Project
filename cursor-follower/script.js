5de4d94d1bec22276d541609dbd22613ccde528b
Poison2612 <sahilsolankey1009@gmail.com>
2023-09-08 17:22:53 +0530

    added cursor follwer

---
+++ cursor-follower/script.js
@@ -0,0 +1,8 @@
+var crsr = document.querySelector("#crsr");
+var main = document.querySelector('#main');
+
+
+main.addEventListener("mousemove", function(dets){
+    crsr.style.top = dets.y-20 + "px";
+    crsr.style.left = dets.x-20 + "px";
+});
